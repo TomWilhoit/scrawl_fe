507a9e8f9fdb8c35e36a15373c3bece3a9e7241d
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-05-22 11:30:20 -0600

    Adds current location

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { MapView } from "expo";
-import {  Permissions } from 'expo';
-
-
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentLatitude: null,
+      currentLongitude: null
+    };
+  }
+
+  componentWillMount =  () => {
+      this.getStartLocation();
+  };
+
+  getStartLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          currentLatitude: position.coords.latitude,
+          currentLongitude: position.coords.longitude
+        });
+      },
+      error => console.log(error),
+      { enableHighAccuracy: false, maximumAge: 1000 }
+    );
+    console.log(this.state)
+  };
+
   render() {
+    const { currentLatitude, currentLongitude } = this.state;
     return (
       <MapView
         style={{
           flex: 1
         }}
         initialRegion={{
-          latitude: 39.7392,
-          longitude: -104.9903,
+          latitude: currentLatitude,
+          longitude: currentLongitude,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
         }}
