afde2c1ae6da6a5dddd0ecff80f8f7eb3a02439b
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-05-26 17:52:42 -0600

    Creates icon compatibility

--- Wall.js
+++ Wall.js
@@ -1,16 +1,15 @@
 import React from "react";
-import App from "./App";
 import { Text, View, TouchableOpacity, TextInput } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default class Wall extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       text: "",
-      comments: ['Example comment 1','Example comment 2', 'Example comment 3']
+      comments: ["Example comment 1", "Example comment 2", "Example comment 3"]
     };
   }
-  
 
   onSubmit = () => {
     const newComment = this.state.text;
@@ -47,22 +46,18 @@ export default class Wall extends React.Component {
       <View
         style={{
           flex: 1,
+          textAlign: "left",
           flexDirection: "column",
           justifyContent: "center",
-          backgroundColor: '#006992'
+          backgroundColor: "#006992"
         }}
       >
-          <TouchableOpacity style={{height: 25, alignItems:'center'}} onPress={() => this.props.onPress("home")}>
-          <Text
-            style={{
-              textAlign: 'center',
-              fontWeight: "bold",
-              color: "white",
-              fontSize: 15
-            }}
-          >
-             Click me to go back 
-          </Text>
+        <TouchableOpacity
+          style={{ height: 25, alignItems: "center" }}
+          onPress={() => this.props.onPress("home")}
+          textAlign="left"
+        >
+          <Icon name="angle-left" size={30} color="white" marginRight={25} />
         </TouchableOpacity>
         <View
           style={{
@@ -79,13 +74,19 @@ export default class Wall extends React.Component {
             height: 100,
             backgroundColor: "white",
             width: "100%",
-            textAlign: "center",
+            textAlign: "center"
           }}
           placeholder="Say something, anything bruh it's your world."
           onChangeText={text => this.setState({ text })}
         />
         <TouchableOpacity
-          style={{ height: 100, backgroundColor: "#87b38d", width: "100%", justifyContent: 'center', marginBottom: -57 }}
+          style={{
+            height: 100,
+            backgroundColor: "#87b38d",
+            width: "100%",
+            justifyContent: "center",
+            marginBottom: -57
+          }}
           onPress={() => this.onSubmit("home")}
         >
           <Text
